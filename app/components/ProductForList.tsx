8c9bfa5119735e89281692dcdcf8fa631457a224
dera <derapalne@gmail.com>
2023-10-11 10:47:41 -0300

    Bugfix

--- app/components/ProductForList.tsx
+++ app/components/ProductForList.tsx
@@ -1,10 +1,14 @@
 "use client";
 import { Product as IProduct } from "../interfaces/Product";
+import { Brand as IBrand } from "../interfaces/Brand";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function ProductForList({ params }: { params: IProduct }) {
-    const { id, name, image_url, price, brand } = params;
+    const { id, name, image_url, price } = params;
+    let brand: IBrand;
+    if (!params.brand) brand = { id: 0, name: "", logo_url: "" };
+    else brand = params.brand;
     return (
         <Link
             className="flex flex-col p-1 shadow rounded-lg cursor-pointer duration-300 dark:border-0 dark:bg-stone-900 hover:bg-stone-200/[0.3] dark:hover:bg-stone-800"

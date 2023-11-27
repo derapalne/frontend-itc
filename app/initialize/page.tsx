59ebf3f069106590a9e4f4bbfa81781f70c999e6
dera <derapalne@gmail.com>
2023-11-27 20:59:46 -0300

    Update Init page to send User Token

--- app/initialize/page.tsx
+++ app/initialize/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 
-async function postInitialization() {
+async function postInitialization(accessToken: string) {
+    console.log(accessToken);
     const response = await fetch(`${process.env["NEXT_PUBLIC_BACKEND_URL"]}/init`, {
         method: "POST",
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
     });
     const jsonResponse = await response.json();
     return jsonResponse;
@@ -12,15 +17,21 @@ async function postInitialization() {
 
 export default function InitializePage() {
     const [firstRender, setFirstRender] = useState(true);
+    const [accessToken, setAccessToken] = useState("");
     const router = useRouter();
 
+    useEffect(() => {
+        const accessTokenCookie = Cookies.get("access_token");
+        if (accessTokenCookie) setAccessToken(accessTokenCookie);
+    }, []);
+
     async function initialize() {
-        const response = await postInitialization();
+        const response = await postInitialization(accessToken);
         console.log(response);
         router.push("/products");
     }
 
-    if (firstRender) {
+    if (firstRender && accessToken) {
         initialize();
         setFirstRender(false);
     }
